b36e154da7e762f5fb67ccda24108f6d670201fe
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-02-03 11:17:04 -0500

    notas en el sistemas pero aun no cargadas en la vista

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,6 +11,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
+import { startLoadingNotes } from '../actions/notes';
 
 export const AppRouter = () => {
     const dispatch = useDispatch();
@@ -18,10 +19,11 @@ export const AppRouter = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     useEffect(() => {
-        firebase.auth().onAuthStateChanged((user)=>{
+        firebase.auth().onAuthStateChanged( async (user)=>{
             if (user?.uid) {
                 dispatch(login (user.uid, user.displayName))
-                setIsLoggedIn(true)
+                setIsLoggedIn(true);
+                dispatch(startLoadingNotes(user.uid));
             }else{
                 setIsLoggedIn(false)
             }
