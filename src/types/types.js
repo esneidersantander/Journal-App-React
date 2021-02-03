5bf938f6f4c1c49701443d8c1a04b8dfa8704c98
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-02-03 10:42:20 -0500

    primera nota subida

--- src/types/types.js
+++ src/types/types.js
@@ -7,5 +7,12 @@ export const types ={
     uiRemoveError:'[UI] Remove Error',
 
     uiStartLoading: '[UI] Start loading',
-    uiFinishLoading: '[UI] Finish loading'
+    uiFinishLoading: '[UI] Finish loading',
+
+    notesAddNew:'[Notes] New note',
+    notesActive:'[Notes] Set Active note',
+    notesLoad:'[Notes] Load notes',
+    notesUpdated:'[Notes] Update note saved',
+    notesDelete:'[Notes] Delete note',
+    notesLogoutCleaning:'[Notes] Logout Cleaning',
 }
